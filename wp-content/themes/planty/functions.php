3f5a3654b89ab2e511fce52c38655b94189b1257
Louise Chartier <louise-chartier@outlook.fr>
2023-10-23 12:22:55 +0200

    css mobile & tablet

--- wp-content/themes/planty/functions.php
+++ wp-content/themes/planty/functions.php
@@ -1,6 +1,18 @@
 <?php
 
 add_action('wp_enqueue_scripts', 'theme_enqueue_styles');
-function theme_enqueue_styles(){
+function theme_enqueue_styles()
+{
     wp_enqueue_style('parent-style', get_template_directory_uri() . '/style.css');
+    wp_enqueue_style('theme-style', get_stylesheet_directory_uri() . '/css/theme.css', array(), filemtime(get_stylesheet_directory() . '/css/theme.css'));
+}
+
+add_filter( 'wp_nav_menu_items', 'link_in_menu');
+
+function link_in_menu( $items) {
+      if (is_user_logged_in()) {
+         $items .= '<li class="menu-item" id="link_menu_admin"> <a href="wp-admin/index.php">Admin</a></li>';
+      } 
+
+   return $items;
 }
